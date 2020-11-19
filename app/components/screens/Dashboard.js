39f95ee433c28b001bbfa366abce5d7f26f64133
yahm23 <yahmed1996a@gmail.com>
2020-11-19 11:42:48 +0000

    able to navigate log in to main dashboard screen, still working on loading

--- app/components/screens/Dashboard.js
+++ app/components/screens/Dashboard.js
@@ -1,11 +1,39 @@
 import React from 'react'
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View,TouchableOpacity, StyleSheet} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import RNRestart from 'react-native-restart'; // Import package from node modules
 
-export default function Dashboard() {
+export default function Dashboard(props) {
+
+    const LogOut = async () => {
+        // props.setLoading(true)
+        try {
+            await AsyncStorage.setItem('LoggedIn', "false")
+            console.log('setting to false');
+            RNRestart.Restart();
+            
+        } catch (e) {
+            // props.setLoading(false)
+            console.log(e);
+        }
+    }
 
     return (
         <View style={styles.body}>
             <Text style={styles.paragraph}>Dashboard</Text>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style ={
+                    {alignSelf: 'center',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: 80,
+                    width: '92%',
+                    borderRadius: 10,
+                    backgroundColor: "#F70B5E"}}
+                    title='Get Started' onPress={()=>LogOut()}>
+                    <Text >Log Out</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
