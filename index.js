628d0bbb0dc6c1f19dd167b76f2f27e96e11d227
yahm23 <yahmed1996a@gmail.com>
2020-11-19 12:14:27 +0000

    cleaned up file tree

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 import {AppRegistry} from 'react-native';
 import App from './app/App';
 import 'react-native-gesture-handler';
-import Screen1 from './app/components/screens/screen1';
 import {name as appName} from './app.json';
 
 // AppRegistry.registerComponent(appName, () => Screen1);
