44f8c52b2eef8c3474d718241f7c7845c9ae8c7e
Green Creative <greencreative@Dans-iMac.local>
2020-11-19 12:37:56 +0000

    Moved loggout option to account

--- app/components/screens/Account.js
+++ app/components/screens/Account.js
@@ -1,11 +1,38 @@
 import React from 'react'
-import {Text, View, StyleSheet} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import RNRestart from 'react-native-restart'; // Import package from node modules
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
 export default function Account() {
+
+    const LogOut = async () => {
+        // props.setLoading(true)
+        try {
+            await AsyncStorage.setItem('LoggedIn', "false")
+            console.log('setting to false');
+            RNRestart.Restart();
+            
+        } catch (e) {
+            // props.setLoading(false)
+            console.log(e);
+        }
+    }
 
     return (
         <View style={styles.body}>
             <Text style={styles.paragraph}>Account</Text>
+
+            <TouchableOpacity style ={
+                    {alignSelf: 'center',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: 80,
+                    width: '92%',
+                    borderRadius: 10,
+                    backgroundColor: "#F70B5E"}}
+                    title='Get Started' onPress={()=>LogOut()}>
+                    <Text >Log Out</Text>
+                </TouchableOpacity>
         </View>
     )
 }
