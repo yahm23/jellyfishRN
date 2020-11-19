804a9cecb7389c39ebd8351c5a8dcb105645bd2a
Green Creative <greencreative@Dans-iMac.local>
2020-11-19 10:32:25 +0000

    Added logo to dashboard

--- app/components/screens/Dashboard.js
+++ app/components/screens/Dashboard.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Image} from 'react-native';
+import logo from '../../images/Jellyfish-white.png';
 
 export default function Dashboard() {
 
     return (
         <View style={styles.body}>
-            <Text style={styles.paragraph}>Dashboard</Text>
+            <Image source={logo} style={styles.logo}/>
+            {/* <Text style={styles.paragraph}>Dashboard</Text> */}
         </View>
     )
 }
@@ -14,9 +16,13 @@ const styles = StyleSheet.create({
     body: {
         height: '100%',
         width: '100%',
-        backgroundColor: '#1A1A1A',
-        justifyContent: 'center',
-        alignItems: 'center'
+        backgroundColor: '#1A1A1A'
+    },
+    logo: {
+        marginTop: 80,
+        height: 40,
+        width: 'auto',
+        resizeMode: 'contain',
     },
     paragraph: {
         color: 'white',
