3e40690f1377628e178f38477b4d13a03662a2ad
Daniel Richards <Kordifier@Gmail.com>
2020-11-22 19:02:35 +0000

    Changed statusbar

--- app/App.js
+++ app/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faChartLine, faChartPie, faChartNetwork, faPoundSign, faUserAstronaut, faProjectDiagram } from '@fortawesome/free-solid-svg-icons';
-
+import { StatusBar } from 'react-native';
 import Dashboard from './components/screens/Dashboard';
 import Appliances from './components/screens/Appliances';
 import Reports from './components/screens/Reports';
@@ -10,36 +10,39 @@ import Offers from './components/screens/Offers';
 import Account from './components/screens/Account';
 import { createAppContainer } from 'react-navigation';
 
+StatusBar.setBarStyle('light-content', true);
+
 const TabNavigator = createBottomTabNavigator(
+    
     {
         Dashboard: {
             screen: Dashboard,
             navigationOptions: {
-                tabBarIcon: () => <FontAwesomeIcon icon={faChartPie} key={'icon_1'}/>
+                tabBarIcon: () => <FontAwesomeIcon size={25} icon={faChartPie} key={'icon_1'}/>
             }
         },
         Appliances: {
             screen: Appliances,
             navigationOptions: {
-                tabBarIcon: () => <FontAwesomeIcon icon={faProjectDiagram} key={'icon_2'}/>
+                tabBarIcon: () => <FontAwesomeIcon size={25} icon={faProjectDiagram} key={'icon_2'}/>
             }
         },
         Reports: {
             screen: Reports,
             navigationOptions: {
-                tabBarIcon: () => <FontAwesomeIcon icon={faChartLine} key={'icon_3'}/>
+                tabBarIcon: () => <FontAwesomeIcon size={25} icon={faChartLine} key={'icon_3'}/>
             }
         },
         Offers: {
             screen: Offers,
             navigationOptions: {
-                tabBarIcon: () => <FontAwesomeIcon icon={faPoundSign} key={'icon_4'}/>
+                tabBarIcon: () => <FontAwesomeIcon size={25} icon={faPoundSign} key={'icon_4'}/>
             }
         },
         Account: {
             screen: Account,
             navigationOptions: {
-                tabBarIcon: () => <FontAwesomeIcon icon={faUserAstronaut} key={'icon_5'}/>
+                tabBarIcon: () => <FontAwesomeIcon size={25} icon={faUserAstronaut} key={'icon_5'}/>
             }
         },
     },
