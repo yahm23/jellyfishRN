c03d3361cc2df3f81fff3b2921f669428078390d
Green Creative <greencreative@Dans-iMac.local>
2020-11-23 16:02:27 +0000

    Removed commented code

--- app/components/screens/Appliances.js
+++ app/components/screens/Appliances.js
@@ -7,16 +7,6 @@ import TileWidget from '../TileWidget';
 
 export default function Appliances() {
 
-    // const TileWidget = (props) => {
-    //     return (
-    //         <View style={styles.widget}>
-    //             <Image style={styles.widgetImage} source={findIcon(props.name) !== null ? findIcon(props.name) : questionMark} />
-    //             <Text style={styles.widgetText}>{props.name}</Text>
-    //             <Image style={styles.arrow} source={arrowIcon} />
-    //         </View>
-    //     )
-    // }
-
     return (
         <View style={styles.body}>
             <Image style={styles.logo} source={JellyLogo} />
