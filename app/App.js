9237f77fece008295adb4c93f821540b7de197fa
Green Creative <greencreative@Dans-iMac.local>
2020-11-24 14:11:16 +0000

    Converted back to destructured navigation

--- app/App.js
+++ app/App.js
@@ -9,40 +9,50 @@ import Offers from './components/screens/Offers';
 import Account from './components/screens/Account';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createAppContainer } from 'react-navigation';
 
 StatusBar.setBarStyle('light-content', true);
 
-export default function App() {
-    const Tab = createBottomTabNavigator();
-
-    return (
-        <NavigationContainer>
-            <Tab.Navigator
-                initialRouteName="Feed"
-                tabBarOptions={{
-                    activeTintColor: '#F70B5E',
-                    style: styles.container
-                }}>
-                <Tab.Screen name="Dashboard" component={Dashboard} options={{
-                    tabBarIcon: ({ color, size }) => <FontAwesomeIcon size={size} icon={faChartPie} color={color} key={'icon_1'} />
-                }} />
-                <Tab.Screen name="Appliances" component={Appliances} options={{
-                    tabBarIcon: ({ color, size }) => <FontAwesomeIcon size={size} icon={faProjectDiagram} color={color} key={'icon_2'} />
-                }} />
-                <Tab.Screen name="Reports" component={Reports} options={{
-                    tabBarIcon: ({ color, size }) => <FontAwesomeIcon size={size} icon={faChartLine} color={color} key={'icon_3'} />
-                }} />
-                <Tab.Screen name="Offers" component={Offers} options={{
-                    tabBarIcon: ({ color, size }) => <FontAwesomeIcon size={size} icon={faPoundSign} color={color} key={'icon_4'} />
-                }} />
-                <Tab.Screen name="Account" component={Account} options={{
-                    tabBarIcon: ({ color, size }) => <FontAwesomeIcon size={size} icon={faUserAstronaut} color={color} key={'icon_5'} />
-                }} />
-            </Tab.Navigator>
-        </NavigationContainer>
-    )
-}
+const MainTabs = createBottomTabNavigator(
+    {
+        Dashboard: {
+            screen: Dashboard,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => <FontAwesomeIcon size={22} icon={faChartPie} color={tintColor} key={'icon_1'} />
+            }
+        },
+        Appliances: {
+            screen: Appliances,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => <FontAwesomeIcon size={22} icon={faProjectDiagram} color={tintColor} key={'icon_2'} />
+            }
+        },
+        Reports: {
+            screen: Reports,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => <FontAwesomeIcon size={22} icon={faChartLine} color={tintColor} key={'icon_3'} />
+            }
+        },
+        Offers: {
+            screen: Offers,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => <FontAwesomeIcon size={22} icon={faPoundSign} color={tintColor} key={'icon_4'} />
+            }
+        },
+        Account: {
+            screen: Account,
+            navigationOptions: {
+                tabBarIcon: ({tintColor }) => <FontAwesomeIcon size={22} icon={faUserAstronaut} color={tintColor} key={'icon_5'} />
+            }
+        }
+    },
+    {
+        tabBarOptions: {
+            activeTintColor: '#F70B5E'
+        }
+    }
+);
 
 const styles = StyleSheet.create({
     container: {
@@ -50,3 +60,5 @@ const styles = StyleSheet.create({
         borderTopWidth: 0
     }
 })
+
+export default createAppContainer(MainTabs);
--- app/components/TileWidget.js
+++ app/components/TileWidget.js
@@ -37,6 +37,6 @@ const styles = StyleSheet.create({
         color: 'white',
         fontSize: 16
     }
-})
+});
 
 export default TileWidget;
