3b82497a6d9607a10a550ea7d1cc2bb357310099
Green Creative <greencreative@Dans-iMac.local>
2020-11-24 16:43:05 +0000

    Stack problems

--- app/components/screens/Appliances.js
+++ app/components/screens/Appliances.js
@@ -1,42 +1,26 @@
-import React, {useEffect, useState} from 'react'
-import { Animated, View, ScrollView,Dimensions, StyleSheet, Image } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { Animated, View, ScrollView, Dimensions, StyleSheet, Image } from 'react-native';
 import brain from '../brain';
 import SearchBar from '../SearchBar';
 import JellyLogo from '../../images/brand/Jellyfish-white.png'
 import TileWidget from '../TileWidget';
 
 export default function Appliances() {
-    const[scrollY, setScrollY] = useState(new Animated.Value(0))
-    // const[headerHeight, setHeaderHeight]= useState(300)
-
-
+    const [scrollY, setScrollY] = useState(new Animated.Value(0))
     const HEADER_EXPANDED_HEIGHT = 100
     const HEADER_COLLAPSED_HEIGHT = 10
     const { width: SCREEN_WIDTH } = Dimensions.get('screen')
-
-    // setHeaderHeight(
-    //     scrollY.interpolate({
-    //         inputRange: [0, HEADER_EXPANDED_HEIGHT-HEADER_COLLAPSED_HEIGHT],
-    //         outputRange: [HEADER_EXPANDED_HEIGHT, HEADER_COLLAPSED_HEIGHT],
-    //         extrapolate: 'clamp'
-    //       })
-    // )
     const headerHeight = scrollY.interpolate({
-      inputRange: [0, HEADER_EXPANDED_HEIGHT-HEADER_COLLAPSED_HEIGHT],
-      outputRange: [HEADER_EXPANDED_HEIGHT, HEADER_COLLAPSED_HEIGHT],
-      extrapolate: 'clamp'
+        inputRange: [0, HEADER_EXPANDED_HEIGHT - HEADER_COLLAPSED_HEIGHT],
+        outputRange: [HEADER_EXPANDED_HEIGHT, HEADER_COLLAPSED_HEIGHT],
+        extrapolate: 'clamp'
     })
-    // useEffect(() => {
-        
-        
-    // }, [scrollY])
-
 
     return (
         <View style={styles.body}>
-            
-            <Animated.View 
-            style={{transform: [{ translateY: headerHeight }], position: 'absolute', top: 0, left: 0 }} 
+
+            <Animated.View
+                style={{ transform: [{ translateY: headerHeight }], position: 'absolute', top: 0, left: 0 }}
             >
                 <Image style={styles.logo} source={JellyLogo} />
                 <View style={styles.searchBarContainer}>
@@ -44,15 +28,16 @@ export default function Appliances() {
                 </View>
             </Animated.View>
 
-            <Animated.ScrollView 
-                style={{paddingTop: HEADER_EXPANDED_HEIGHT}}
+            <Animated.ScrollView
+                style={{ paddingTop: HEADER_EXPANDED_HEIGHT }}
                 onScroll={Animated.event(
-                    [{ nativeEvent: {
-                        contentOffset: {
-                            y: scrollY
+                    [{
+                        nativeEvent: {
+                            contentOffset: {
+                                y: scrollY
+                            }
                         }
-                    }
-                    }],{useNativeDriver: true}
+                    }], { useNativeDriver: true }
                 )}
                 scrollEventThrottle={16}
             >
@@ -60,7 +45,7 @@ export default function Appliances() {
 
                 {brain.map((room, index) => {
                     return (
-                        <TileWidget name={room.name} key={index}/>
+                        <TileWidget name={room.name} key={index} />
                     )
                 })}
             </Animated.ScrollView>
@@ -75,7 +60,6 @@ const styles = StyleSheet.create({
         backgroundColor: '#1A1A1A'
     },
     logo: {
-        // marginTop: 80,
         height: 35,
         width: 35,
         resizeMode: 'contain',
@@ -88,8 +72,5 @@ const styles = StyleSheet.create({
         color: 'white',
         fontSize: 16,
         textAlign: 'center'
-    },
-    widgetContainer: {
-        // marginTop: 40
     }
 })
